2590877dc4b691cd6f69b08487c21872a8ba2c00
Harsh-Kumar-26 <harshkumar010377@gmail.com>
2025-12-04 09:55:19 +0530

    Openrelay Turn server

--- client/src/services/peer.js
+++ client/src/services/peer.js
@@ -19,6 +19,21 @@ class PeerService {
               "stun:stun1.l.google.com:3478",
             ],
           },
+          {
+            urls: "turn:openrelay.metered.ca:80",
+            username: "openrelayproject",
+            credential: "openrelayproject",
+          },
+          {
+            urls: "turn:openrelay.metered.ca:443",
+            username: "openrelayproject",
+            credential: "openrelayproject",
+          },
+          {
+            urls: "turn:openrelay.metered.ca:443?transport=tcp",
+            username: "openrelayproject",
+            credential: "openrelayproject",
+          }
         ],
       });
 
