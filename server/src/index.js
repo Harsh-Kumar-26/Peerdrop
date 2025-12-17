b66ad65ea3078d0dd121408687922b95b345422f
Harsh-Kumar-26 <harshkumar010377@gmail.com>
2025-12-17 11:05:57 +0530

    Health added

--- server/src/index.js
+++ server/src/index.js
@@ -6,6 +6,11 @@ import {initSocket} from "./socket.js";
 
 dotenv.config();
 
+app.get("/health", (req, res) => {
+  res.status(200).send("OK");
+});
+
+
 const server = createServer(app);
 console.log("Socket server created");
 
